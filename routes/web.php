7b1ddab2ed2e56c2fb690663004968407744dcb8
RyuuYZ <ryuuyz09@gmail.com>
2026-02-18 23:41:14 +0700

    Refactor routes to use HomeController and DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    // use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+    // Route::get('/', function () {
+    // return view('welcome');
+    // });
+    // use App\Http\Controllers\Frontend\HomeController;
+    // Route::get('/', [HomeController::class, 'index']);
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 });
+
